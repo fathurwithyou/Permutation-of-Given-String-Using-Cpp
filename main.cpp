ccc97c137b35fc46ac1907c3e0be31f957299156
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2022-09-25 23:15:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
 #include <bits/stdc++.h>
-#define fathurwithyou ios::sync_with_stdio(false); cin.tie(0); 
+#define fathurwithyou            \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);
 
 typedef long long ll;
 typedef unsigned long long ull;
 using namespace std;
 
-ll fact(ll n){
-	ll f = 1;
-	for (int i = 1; i <= n; i++){
-		f *= i;
-	}
-	return f;
+ll fact(ll n) {
+    ll f = 1;
+    for (int i = 1; i <= n; i++) {
+        f *= i;
+    }
+    return f;
 }
 
 void solve() {
-	map<char, ll> str;
-    string s; cin >> s;
+    map<char, ll> str;
+    string s;
+    cin >> s;
     ll len = s.size();
     ll res = fact(len);
-    for (int i = 0; i < len; i++){
-    	str[s[i]]++;
-	}
-	for (auto x : str){
-		cout << x.first << ": " << x.second << "\n";
-		res /= fact(x.second);
-	}
-	cout << res;
+    for (int i = 0; i < len; i++) {
+        str[s[i]]++;
+    }
+    for (auto x : str) {
+        cout << x.first << ": " << x.second << "\n";
+        res /= fact(x.second);
+    }
+    cout << res;
     return;
 }
 
 int main() {
-	fathurwithyou
-    solve();
+    fathurwithyou solve();
     return 0;
 }
